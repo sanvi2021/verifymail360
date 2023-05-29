d9bd4d3e010fa76cb24d1e41d0e3b244cad5f481
Nishant Kumar <learnlogix@gmail.com>
2023-05-30 00:00:40 +0530

    changes

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,14 +1,12 @@
-from django.contrib import admin
 from django.urls import path
-# from drf_spectacular.views import (
-#     SpectacularAPIView,
-#     SpectacularRedocView,
-#     SpectacularSwaggerView,
-# )
+from .views import *
+
 
 from .views import SignupViewSet, Activate
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("singup/",SignupViewSet.as_view({"post": "create"}), name= "Signup"),
-    path('activate/<uidb64>/<token>/',Activate.as_view({"get":"retrieve"}), name='activate')
+    path("signup/",SignupViewSet.as_view({"post": "create"}), name= "Signup"),
+    path('activate/',Activate.as_view(), name='activate'),
+    path('login/', UserLoginView.as_view(), name='user_login'),
+    path('reset-password/', UserPasswordResetView.as_view(), name='user_password_reset'),
+    path('logout/', UserLogoutView.as_view(), name='user_logout'),
 ]
--- verifymail/urls.py
+++ verifymail/urls.py
@@ -17,22 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView,
-)
 
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('authentication/',include('authentication.urls'), name = "authentication"),
+    path('core/',include('core.urls'), name = "core"),
+
     # ...
-    # API Schema:
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Optional UI:
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('authentication/',include('authentication.urls'), name = "authentication")
-        # ...
 ]
 
